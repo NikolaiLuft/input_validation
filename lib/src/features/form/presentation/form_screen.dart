35cc318c878625d7e22baecf82563e16da92287f
Nikolai Luft <nikoluft92@gmail.com>
2024-05-24 19:08:59 +0200

    implemented formkey

--- lib/src/features/form/presentation/form_screen.dart
+++ lib/src/features/form/presentation/form_screen.dart
@@ -1,50 +1,78 @@
 import 'package:flutter/material.dart';
+
 class FormScreen extends StatelessWidget {
   // Attribute
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
   // (keine)
   // Konstruktor
-  const FormScreen({super.key});
+  FormScreen({super.key});
   // Methoden
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Form(
-          child: Column(children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                label: Text("Email"),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text("Email"),
+                ),
+                autovalidateMode: AutovalidateMode.always,
+                validator: validateEmail,
               ),
-              autovalidateMode: AutovalidateMode.always,
-              validator: validateEmail,
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                label: Text("Passwort"),
+              const SizedBox(height: 8),
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text("Passwort"),
+                ),
+                autovalidateMode: AutovalidateMode.always,
+                validator: validatePw,
               ),
-              autovalidateMode: AutovalidateMode.always,
-              validator: validatePw,
-            ),
-            const SizedBox(height: 32),
-            FilledButton(
-              onPressed: () {},
-              child: const Text("Login"),
-            ),
-          ]),
+              const SizedBox(height: 32),
+              FilledButton(
+          onPressed: () {
+            if (formKey.currentState?.validate() ?? false) {
+        // Die Form ist valide
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Eingaben sind korrekt!'),
+          ),
+        );
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => FormScreen()));
+            } else {
+        // Die Form ist nicht valide
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Bitte korrigiere deine Eingaben.')),
+        );
+            }
+          },
+          child: const Text("Login"),
+        ),
+            ]),
+          ),
         ),
       ),
     );
   }
+
   String? validateEmail(String? input) {
     if (input == null || input.isEmpty) {
       return 'E-Mail darf nicht leer sein';
     }
-    if (input.length <= 5) {
-      return 'E-Mail muss mehr als 5 Zeichen haben';
+    if (input.length <= 6) {
+      return 'E-Mail muss mehr als 6 Zeichen haben';
     }
     if (!input.contains('@')) {
       return 'E-Mail muss ein "@" enthalten';
@@ -54,6 +82,7 @@ class FormScreen extends StatelessWidget {
     }
     return null;
   }
+
   String? validatePw(String? input) {
     if (input == null || input.isEmpty) {
       return 'Passwort darf nicht leer sein';
@@ -63,4 +92,4 @@ class FormScreen extends StatelessWidget {
     }
     return null;
   }
-} 
+}
